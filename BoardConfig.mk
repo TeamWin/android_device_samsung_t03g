1fa6c01315c1b36fca245bedf631eb56cc6eda04
bigbiff bigbiff <bigbiff@teamw.in>
2013-04-17 09:38:13 -0400

    twrp2.5

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -85,3 +85,4 @@ TW_CRYPTO_KEY_LOC := "footer"
 TW_BRIGHTNESS_PATH := "/sys/devices/platform/s5p-dsim.0/s6evr02/backlight/panel/brightness"
 TW_MAX_BRIGHTNESS := 255
 TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/virtual/android_usb/android0/f_mass_storage/lun0/file
